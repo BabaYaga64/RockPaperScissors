706ff6a1a2220efd28f3b2df7a867c4775ffb5b5
Bojana Skarich & Liz Beacham <student@epicodus.com>
2015-03-11 10:19:22 -0700

    Add initial files to set up app and prep for testing.

---
+++ src/RockPaperScissors.php
@@ -0,0 +1,16 @@
+<?php
+
+    class RockPaperScissors
+    {
+
+        function rockPaperScissors($first_input, $second_input);
+        {
+            //Function goes here for draw result when both inputs match
+        }
+
+
+    }
+
+
+
+?>
