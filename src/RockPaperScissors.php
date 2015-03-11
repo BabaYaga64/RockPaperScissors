7d86439c73addb2bf220f9637289677e6f5cc6e9
Bojana Skarich & Liz Beacham <student@epicodus.com>
2015-03-11 11:21:44 -0700

    Add passing rock-scissors test to Test file, with corresponding method add to class declaration.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -8,6 +8,10 @@
             if ($first_input == $second_input) {
                 return "It's a draw!";
             }
+
+            if ($first_input == "rock" && $second_input == "scissors") {
+                return "Player 1 wins!";
+            }
         }
     }
 ?>
