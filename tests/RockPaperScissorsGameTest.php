51c11d1a91bf2d0279181869c6a8f37aa4a5c9c0
Bojana Skarich & Liz Beacham <student@epicodus.com>
2015-03-11 11:11:17 -0700

    Add test for rock and scissors and test fail because method has not been written.

--- tests/RockPaperScissorsGameTest.php
+++ tests/RockPaperScissorsGameTest.php
@@ -17,5 +17,52 @@
             //Assert
             $this->assertEquals("It's a draw!", $result);
         }
+
+        function test_paper_paper()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $first_input = "paper";
+            $second_input = "paper";
+
+            //Act
+            $result = $test_RockPaperScissors->rockPaperScissorsGame($first_input, $second_input);
+
+
+            //Assert
+            $this->assertEquals("It's a draw!", $result);
+        }
+
+        function test_scissors_scissors()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $first_input = "scissors";
+            $second_input = "scissors";
+
+            //Act
+            $result = $test_RockPaperScissors->rockPaperScissorsGame($first_input, $second_input);
+
+
+            //Assert
+            $this->assertEquals("It's a draw!", $result);
+        }
+
+
+        function test_rock_scissors()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $first_input = "rock";
+            $second_input = "scissors";
+
+            //Act
+            $result = $test_RockPaperScissors->rockPaperScissorsGame($first_input, $second_input);
+
+            //Assert
+            $this->assertEquals("Player 1 wins!", $result);
+        }
+
+
     }
 ?>
